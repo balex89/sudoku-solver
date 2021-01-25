aded039b1a0c03d63981907ba78d952ceb5d9644
abobylev <abobylev@inleksys.ru>
2021-01-25 18:10:28 +0300

    SU-5 Added initial modules

---
+++ cell.py
@@ -0,0 +1,8 @@
+class Cell:
+    # TODO: implement
+
+    def __init__(self):
+        self.alternatives = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+    def is_solved(self) -> bool:
+        pass
---
+++ solver.py
@@ -0,0 +1,16 @@
+import sys
+
+from sudoku import Sudoku
+import utils
+
+# TODO implement
+
+
+def solve_file(file_path: str):
+    # TODO implement using utils.read/write_grid_from/to_file
+    pass
+
+
+if __name__ == "__main__":
+    file_path = sys.argv[0]
+    solve_file(file_path)

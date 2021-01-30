ac5bdbfa82624ad516907d44188ec4d892552e99
Kalinov Sergey <kalinovss@gmail.com>
2021-01-30 23:09:59 +0400

    SUD-22 function read_grid_from_file changed(balex89 changes request)

--- utils.py
+++ utils.py
@@ -1,21 +1,25 @@
 def read_grid_from_file(file_path):
-    f = open(file_path, "r")                         # открываем файл
-    text_all = f.readlines()                         # считываем все строки в список
-    f.close()                                        # закрываем файл
-    valid_chars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", " "]  # список допустимых символов
-    m_final = []                                     # итоговый список
-
-    for row in text_all:                             # перебор всех считанных строк
-        m_temp = []                                  # в этот список добавляем допустимые символы из текущей строки
-        for char in row:                             # пербор символов в текущей строке
-            if char in valid_chars:                  # если допустимый символ, добавляем его в список
-                if char != " ":
-                    m_temp.append(int(char))
-                else:
-                    m_temp.append(None)
-        if len(m_temp) > 0:                          # если список не пустой (в строке есть допустимые символы)
-            m_final.append(m_temp)                   # добавляем список в итоговый список
-    return m_final
+    if  not isinstance(file_path,str):                          #если аргумент  file_path не строка
+        raise TypeError("Invalid argument type: excepted str")  #поднимаем ошибку
+        return                                                  #и выходим из функции
+    with open(file_path, "r", encoding="utf8") as f:            # открываем файл
+        text_all = f.readlines()                                # считываем все строки в список
+                           
+    VALID_CHARS = ("1", "2", "3", "4", "5", "6", "7", "8", "9")  # кортеж допустимых символов, кроме символа пустой клетки
+    EMPTY_CELL_CHAR = " "                                        #символ пустой клетки
+    grid_final = []                                              # итоговый список
+    decode_cell_char = lambda c: int(c) if c != EMPTY_CELL_CHAR else None  #функция возвращает валидный символ, либо цифру либо None
+    is_valid_char = lambda c: True if c in VALID_CHARS or c == EMPTY_CELL_CHAR else False #функция проверяет является ли символ валидным
+    
+    for row in text_all:                                                       # перебор всех считанных строк
+        grid_row = [decode_cell_char(char) for char in row if is_valid_char(char)]  # если допустимый символ, добавляем его в список
+        if len(grid_row) == 9:                                                 # если в списке ровно 9 символов
+            grid_final.append(grid_row)                                        # добавляем список в итоговый список
+    
+    if len(grid_final) == 9:                                                   #если в итоговом списке 9 строк
+        return grid_final                                                      #возвращаем список
+    else:
+        raise ValueError("Invalid input file: expected 9 rows of 9 cells") #поднимаем ошибку
 
 
 def write_grid_to_file(grid, file_path):

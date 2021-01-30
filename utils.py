c40777af9f64f8547ed758c41b8cfaeec29972d9
JanPyton <boxmail696@gmail.com>
2021-01-30 22:01:30 +0300

    write in file

--- utils.py
+++ utils.py
@@ -3,4 +3,7 @@ def read_grid_from_file(file_path):
 
 
 def write_grid_to_file(grid, file_path):
-    pass  # TODO: implement
+    f = open("file_path.txt", "w", encoding='utf-8')
+    for line in [''.join(i) for i in grid]:
+        f.write(line + '\n')
+    f.close()

5bb44d43012f754837a0b38c6a0fbb3c85e51fdc
Kalinov Sergey <kalinovss@gmail.com>
2021-01-31 10:51:38 +0400

    function read_grid_from_file changed(balex89 changes request#2)

--- utils.py
+++ utils.py
@@ -1,18 +1,15 @@
+EMPTY_CELL_CHAR = " "                                        #символ пустой клетки
+VALID_MUMBERS = ("1", "2", "3", "4", "5", "6", "7", "8", "9")  # кортеж допустимых символов, кроме символа пустой клетки
+VALID_CHARS = (*VALID_MUMBERS,EMPTY_CELL_CHAR)
+_decode_cell_char_ = lambda c: int(c) if c != EMPTY_CELL_CHAR else None  #функция возвращает валидный символ, либо цифру либо None
+
 def read_grid_from_file(file_path):
-    if  not isinstance(file_path,str):                          #если аргумент  file_path не строка
-        raise TypeError("Invalid argument type: excepted str")  #поднимаем ошибку
-        return                                                  #и выходим из функции
-    with open(file_path, "r", encoding="utf8") as f:            # открываем файл
-        text_all = f.readlines()                                # считываем все строки в список
-                           
-    VALID_CHARS = ("1", "2", "3", "4", "5", "6", "7", "8", "9")  # кортеж допустимых символов, кроме символа пустой клетки
-    EMPTY_CELL_CHAR = " "                                        #символ пустой клетки
-    grid_final = []                                              # итоговый список
-    decode_cell_char = lambda c: int(c) if c != EMPTY_CELL_CHAR else None  #функция возвращает валидный символ, либо цифру либо None
-    is_valid_char = lambda c: True if c in VALID_CHARS or c == EMPTY_CELL_CHAR else False #функция проверяет является ли символ валидным
     
+    with open(file_path, "r", encoding="utf8") as f:                          # открываем файл
+        text_all = f.readlines()                                               # считываем все строки в список
+    grid_final = []                                                            # итоговый список
     for row in text_all:                                                       # перебор всех считанных строк
-        grid_row = [decode_cell_char(char) for char in row if is_valid_char(char)]  # если допустимый символ, добавляем его в список
+        grid_row = [_decode_cell_char_(char) for char in row if char in VALID_CHARS ]  # если допустимый символ, добавляем его в список
         if len(grid_row) == 9:                                                 # если в списке ровно 9 символов
             grid_final.append(grid_row)                                        # добавляем список в итоговый список
     

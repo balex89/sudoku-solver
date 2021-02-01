e3e94aa0fdb26f27106f31732722ebf6ebb00e43
JanPyton <boxmail696@gmail.com>
2021-02-01 20:57:30 +0300

    SUD-23 writing the result to a file

--- utils.py
+++ utils.py
@@ -1,9 +1,23 @@
+EMPTY_CELL_CHAR = " "
+
+
+def to_valid_char(value):
+    if value==None
+        return EMPTY_CELL_CHAR
+    else
+        return str(value)
+
+
 def read_grid_from_file(file_path):
     pass  # TODO: implement
 
 
 def write_grid_to_file(grid, file_path):
-    f = open("file_path.txt", "w", encoding='utf-8')
-    for line in [''.join(i) for i in grid]:
-        f.write(line + '\n')
-    f.close()
+
+    with open(file_path, "w", encoding="utf8") as f:  # открываем файл
+        for sp in grid:
+            itog_sp = [to_valid_char(value) for value in sp]
+            line='|',join(itog_sp)
+            f.write(line + '\n')
+            f.write("---------" + '\n')
+

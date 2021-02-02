85d1b5046afd3062601196141dc1a68c5caded9e
JanPyton <boxmail696@gmail.com>
2021-02-02 23:24:23 +0300

    Sud 23 writing the result to a file

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ EMPTY_CELL_CHAR = " "                                         # констант
 
 
 def to_valid_char(value):                                     # функция приводит значения в списке grid к символам
-    if value == None:                                         # если значение None, то возвращает пробел
+    if value is None:                                         # если значение None, то возвращает пробел
         return EMPTY_CELL_CHAR
     else:                                                     # в противном случае возвращает символ значения (числа)
         return str(value)
@@ -13,11 +13,8 @@ def read_grid_from_file(file_path):
 
 
 def write_grid_to_file(grid, file_path):                      # функция записи результата решения Судоку в файл
-
     with open(file_path, "w", encoding="utf8") as f:          # открываем файл на запись
-        for sp in grid:                                       # перебираем списки списка grid
-            itog_sp = [to_valid_char(value) for value in sp]  # создаем список с приведенными значениями к символам
-            line='|'.join(itog_sp)                            # преобразуем полученный список в строку 
+        for line in grid:                                     # перебираем списки списка grid
+            converted_line = [to_valid_char(value) for value in line]  # создаем список с символами
+            line = ''.join(converted_line)                    # преобразуем полученный список в строку
             f.write(line + '\n')                              # записывам полученную строку в файл
-            f.write("---------" + '\n')                       # записываем горизонтальную разделительную черту в файл
-

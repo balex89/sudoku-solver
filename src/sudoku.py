3f0e02c26a3396d0f2bd9c536205ff648863ca8a
KalinovSergey <77960909+KalinovSergey@users.noreply.github.com>
2021-03-19 17:04:58 +0300

    SUD-51 Add columns and squares views to sudoku class (#15)

--- src/sudoku.py
+++ src/sudoku.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from cell import Cell
 
 
@@ -5,6 +7,10 @@ class Sudoku:
 
     def __init__(self, grid):
         self.__rows = [[Cell(item) for item in row] for row in grid]
+        self.__columns = [[self.__rows[j][i] for j in range(9)] for i in range(9)]
+        self.__squares = []
+        for i, j in product((0, 3, 6), (0, 3, 6)):
+            self.__squares.append([self.__rows[x][y] for x in range(i, i+3) for y in range(j, j+3)])
 
     def solve(self):
         pass  # TODO: implement

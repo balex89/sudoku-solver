1637dd5f538219f58b7bd177371a6a68ed2d5a77
Kalinov Sergey <kalinovss@gmail.com>
2021-04-16 23:16:29 +0400

    SUD-71 Delete auxiliary prints

--- src/sudoku.py
+++ src/sudoku.py
@@ -13,7 +13,7 @@ class Sudoku:
             self.__squares.append([self.__rows[x][y] for x in range(i, i+3) for y in range(j, j+3)])
 
     def exclude_equal_alternatives(self, grid):
-        is_any_cell_solved_ = False  #локалная переменная - флаг что хоть одна клетка решена
+        is_any_cell_solved_ = False  # локалная переменная - флаг что хоть одна клетка решена
         for i in range(9):   # формируем  два массива - одинкакове альтернативы и их количество в пачке
             alternatives = []
             count = []
@@ -23,20 +23,16 @@ class Sudoku:
                     count.append(1)
                 else:
                     count[alternatives.index(grid[i][j].alternatives)] += 1
-            print("row ", str(i), "alternatives:", alternatives)
-            print("row ", str(i), "count:", count)
             exclude_set = set()     # формируем множество для исключения (альтернативы длинной N в количестве N в одной пачке)
             for c in range(len(alternatives)):
                 if len(alternatives[c]) == count[c]:
                     exclude_set = alternatives[c]
-            print("row ", str(i), "exclude_set:", exclude_set)
             if len(exclude_set) > 0:     #если такое множество найдено, исключаем его из всех остальных клеток пачки
                 for e in range(9):
                     if not grid[i][e].is_solved:
                         if grid[i][e].alternatives != exclude_set:
                             grid[i][e].exclude(exclude_set)
                             is_any_cell_solved_ |= grid[i][e].is_solved
-                    print("alternatives after excluding :", grid[i][e].alternatives)
         return is_any_cell_solved_
 
     def solve(self):
@@ -55,7 +51,6 @@ class Sudoku:
                 is_any_cell_solved |= self.exclude_equal_alternatives(self.__columns)
                 is_any_cell_solved |= self.exclude_equal_alternatives(self.__squares)
             if not is_any_cell_solved:
-                print("Кругов:", circus)
                 break
 
     def get_grid(self):

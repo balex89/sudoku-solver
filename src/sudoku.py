b54761515dbb69250ca87eee67bef08b15bdd4da
Kalinov Sergey <kalinovss@gmail.com>
2021-05-04 11:04:29 +0400

    SUD-71 Code refactoring(delete exclude_set)

--- src/sudoku.py
+++ src/sudoku.py
@@ -13,17 +13,15 @@ class Sudoku:
         for i, j in product((0, 3, 6), (0, 3, 6)):
             self.__squares.append([self.__rows[x][y] for x in range(i, i+3) for y in range(j, j+3)])
 
-    def __exclude_equal_alternatives__(self, grid_view):
+    def _exclude_equal_alternatives(self, grid_view):
         is_any_cell_solved = False  # флаг что хоть одна клетка решена
         for i in range(9):
-            alternatives = Counter([grid_view[i][j].alternatives for j in range(9)])  # формируем словарь алтернативы в пачке(строка, столбец или квадрант):количество таких альтернатив в пачке 
-            exclude_set = set()
-            for alternative in alternatives:
-                if len(alternative) == alternatives[alternative]:  # ищем множество для исключения (альтернативы длинной N в количестве N в одной пачке)
-                    exclude_set = alternative
+            twin_alternatives_counter = Counter(grid_view[i][j].alternatives for j in range(9))  # формируем словарь альтернативы в пачке(строка, столбец или квадрант):количество таких альтернатив в пачке 
+            for alternatives in twin_alternatives_counter:
+                if len(alternatives) == twin_alternatives_counter[alternatives]:  # ищем множество для исключения (альтернативы длинной N в количестве N в одной пачке)
                     for n in range(9):
-                        if not grid_view[i][n].is_solved and grid_view[i][n].alternatives != exclude_set:
-                            grid_view[i][n].exclude(exclude_set)
+                        if not grid_view[i][n].is_solved and grid_view[i][n].alternatives != alternatives:
+                            grid_view[i][n].exclude(alternatives)
                             is_any_cell_solved |= grid_view[i][n].is_solved
                     break
         return is_any_cell_solved
@@ -39,7 +37,7 @@ class Sudoku:
                     is_any_cell_solved |= self.__rows[i][j].is_solved
             if not is_any_cell_solved:
                 for grid_view in [self.__rows, self.__columns, self.__squares]:
-                    is_any_cell_solved |= self.__exclude_equal_alternatives__(grid_view)
+                    is_any_cell_solved |= self._exclude_equal_alternatives(grid_view)
             if not is_any_cell_solved:
                 break
 

cdca7c623033e7f89f0c9a93b5f95de8a4b7fea4
Kalinov Sergey <kalinovss@gmail.com>
2021-05-29 11:59:47 +0400

    SUD-76 Add alternatives checking

--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -65,10 +65,9 @@ def test_sudoku_solver_hard_task():
     assert s.get_grid() == HARD_SOLUTION
 
 
-def _cell_from_alternatives(alternatives=None):
+def _cell_from_alternatives(alternatives):
     c = Cell()
-    if alternatives is not None:
-        c._Cell__alternatives = alternatives
+    c._Cell__alternatives = alternatives
     return c
 
 
@@ -79,13 +78,13 @@ def test_exclude_equal_alternatives():
     {1, 2, 3, 4},
     {1, 2, 3, 4},
     {1, 2, 3, 4, 5},
-    {1, 2, 3, 4, 6},
-    {1, 2, 3, 4, 7},
-    {1, 2, 3, 4, 8},
-    {1, 2, 3, 4, 9}
+    {1, 2, 3, 4, 6, 5},
+    {1, 2, 3, 4, 7, 8, 9},
+    {1, 2, 3, 4, 8, 9, 7},
+    {1, 2, 3, 4, 9, 7, 8}
     ]
     EEA_SOLUTION = [
-    [None, None, None, None, 5, 6, 7, 8, 9],
+    [None, None, None, None, 5, None, None, None, None],
     [None, None, None, None, None, None, None, None, None],
     [None, None, None, None, None, None, None, None, None],
     [None, None, None, None, None, None, None, None, None],
@@ -95,6 +94,20 @@ def test_exclude_equal_alternatives():
     [None, None, None, None, None, None, None, None, None],
     [None, None, None, None, None, None, None, None, None]
     ]
-    grid_view = [[_cell_from_alternatives(EEA_1ST_ROW_ALTERNATIVES[i]) if j == 0 else _cell_from_alternatives() for i in range(9)] for j in range(9)]
+    EEA_1ST_ROW_ALTERNATIVES_SOLUTION = [
+    {1, 2, 3, 4},
+    {1, 2, 3, 4},
+    {1, 2, 3, 4},
+    {1, 2, 3, 4},
+    set(),
+    {6, 5},
+    {7, 8, 9},
+    {8, 9, 7},
+    {9, 7, 8}
+    ]  
+
+    grid_view = [[_cell_from_alternatives(EEA_1ST_ROW_ALTERNATIVES[i]) if j == 0 else Cell() for i in range(9)] for j in range(9)]
     Sudoku._Sudoku__exclude_equal_alternatives(grid_view)
-    assert [[cell.value for cell in row] for row in grid_view] == EEA_SOLUTION
+    assert [[cell.value for cell in row] for row in grid_view] == EEA_SOLUTION             # проверяем value во всем гриде: в первой строке 5-ая клетка решена, в других строках value не изменились и равны None
+    assert [cell._Cell__alternatives for cell in grid_view[0]] == EEA_1ST_ROW_ALTERNATIVES_SOLUTION # проверяем альтернативы в первой строке
+    assert all([grid_view[i][j]._Cell__alternatives == {1, 2, 3, 4, 5, 6, 7, 8, 9} for j in range(9)] for i in range(1,9)) # проверяем, что альтернативы в других строках не изменлись

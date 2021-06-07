9f38a204ba0981a2bdb36585ddba81a0229da990
unknown <kalinovss@gmail.com>
2021-06-07 11:07:22 +0300

    SUD-76 Delete assert for cell rosolve

--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -27,29 +27,19 @@ def test_exclude_equal_alternatives():
     {1, 2, 3, 4},
     {1, 2, 3, 4},
     {1, 2, 3, 4},
-    {1, 2, 3, 4, 5},
+    {1, 2, 3, 4, 5, 6},
     {1, 2, 3, 4, 6, 5},
     {1, 2, 3, 4, 7, 8, 9},
     {1, 2, 3, 4, 8, 9, 7},
     {1, 2, 3, 4, 9, 7, 8}
     ]
-    EEA_SOLUTION = [
-    [None, None, None, None, 5, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None]
-    ]
+
     EEA_1ST_ROW_ALTERNATIVES_SOLUTION = [
     {1, 2, 3, 4},
     {1, 2, 3, 4},
     {1, 2, 3, 4},
     {1, 2, 3, 4},
-    set(),
+    {6, 5},
     {6, 5},
     {7, 8, 9},
     {8, 9, 7},
@@ -58,6 +48,5 @@ def test_exclude_equal_alternatives():
 
     grid_view = [[_cell_from_alternatives(EEA_1ST_ROW_ALTERNATIVES[i]) if j == 0 else Cell() for i in range(9)] for j in range(9)]
     Sudoku._Sudoku__exclude_equal_alternatives(grid_view)
-    assert [[cell.value for cell in row] for row in grid_view] == EEA_SOLUTION             # проверяем value во всем гриде: в первой строке 5-ая клетка решена, в других строках value не изменились и равны None
     assert [cell._Cell__alternatives for cell in grid_view[0]] == EEA_1ST_ROW_ALTERNATIVES_SOLUTION # проверяем альтернативы в первой строке
     assert all([grid_view[i][j]._Cell__alternatives == {1, 2, 3, 4, 5, 6, 7, 8, 9} for j in range(9)] for i in range(1,9)) # проверяем, что альтернативы в других строках не изменлись

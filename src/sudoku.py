46076b360088e51c07d3ff0f2b95ea3f5a5e608e
KalinovSergey <77960909+KalinovSergey@users.noreply.github.com>
2021-12-17 16:41:28 +0300

    SUD-149 Fix EEA. Excluding alternatives in list of cell indexes (#47)

--- src/sudoku.py
+++ src/sudoku.py
@@ -73,10 +73,12 @@ class Sudoku:
             twin_alternatives_counter = Counter(batch[j].alternatives for j in range(9))
             for alternatives in twin_alternatives_counter:
                 if len(alternatives) == twin_alternatives_counter[alternatives]:
-                    for n in range(9):
-                        if not batch[n].is_solved and batch[n].alternatives != alternatives:
-                            batch[n].exclude(alternatives)
-                            is_any_cell_solved |= batch[n].is_solved
+                    for index in [
+                        index for index in range(9) if
+                        not batch[index].is_solved and batch[index].alternatives != alternatives
+                    ]:
+                        batch[index].exclude(alternatives)
+                        is_any_cell_solved |= batch[index].is_solved
                     break
         return is_any_cell_solved
 

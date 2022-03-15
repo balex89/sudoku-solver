c6ba796b550043ccc306afefa037d0dd0e25048d
unknown <kalinovss@gmail.com>
2022-03-15 22:28:04 +0300

    SUD-151 Changes in accordance PEP8#2

--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -166,9 +166,9 @@ def test_sudoku_get_task_definite_difficulty():
     sudoku = Sudoku(Sudoku.get_task(-1, 3, 5))
     sudoku.solve()
     assert (
-        sudoku._counter[sudoku._leave_equal_alternatives].solved
-        + sudoku._counter[sudoku._exclude_equal_alternatives].solved
-        ) == 4
+            sudoku._counter[sudoku._leave_equal_alternatives].solved
+           + sudoku._counter[sudoku._exclude_equal_alternatives].solved
+           ) == 4
 
 
 def test_fix_error_empty_alternatives_before_solution():
